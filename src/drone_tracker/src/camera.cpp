2e43e31a04cabdc9914214083b101b5ab06515b1
Davide Morazzo <morazzo.davide@gmail.com>
2023-07-03 17:22:26 +0200

    aruco pose estimation WIP

--- src/drone_tracker/src/camera.cpp
+++ src/drone_tracker/src/camera.cpp
@@ -27,18 +27,22 @@ public:
 			std::string(ros_namespace + "/camera/image_raw"), 10, 
 			std::bind(& Camera::image_cb, this, _1));
 
-		float c_matrix[9] = {381.36246688113556, 0.0, 320.5, 0.0, 381.36246688113556, 240.5, 0.0, 0.0, 1.0};
-		cameraMatrix = cv::Mat(3,3,CV_32F, c_matrix);
-		float dist_vect[5] = {0,0,0,0,0};
-		distCoeffs = cv::Mat(1,5, CV_32F, dist_vect);
-
+		double c_matrix[9] = {381.36246688113556, 0.0, 320.5, 0.0, 381.36246688113556, 240.5, 0.0, 0.0, 1.0};
+		cameraMatrix = cv::Mat(3,3,CV_64FC1, &c_matrix);
+		double dist_vect[5] = {0,0,0,0,0};
+		distCoeffs = cv::Mat(5,1, CV_64FC1, &dist_vect);
+		RCLCPP_INFO(get_logger(), "Camera matrix %f %f %f %f %f %f %f %f %f", cameraMatrix.at<double>(0,0),
+			cameraMatrix.at<double>(0,1), cameraMatrix.at<double>(0,2), cameraMatrix.at<double>(1,0), cameraMatrix.at<double>(1,1), 
+			cameraMatrix.at<double>(1,2), cameraMatrix.at<double>(2,0),cameraMatrix.at<double>(2,1), cameraMatrix.at<double>(2,2));
+		RCLCPP_INFO(get_logger(), "Distorsion  matrix %f %f %f %f %f", distCoeffs.at<double>(0,0),
+			distCoeffs.at<double>(1,0), distCoeffs.at<double>(2,0), distCoeffs.at<double>(3,0), distCoeffs.at<double>(4,0));
 	};
 
 private:
 	cv_bridge::CvImagePtr cv_ptr;
 	std::string ros_namespace;
 	sensor_msgs::msg::Image::SharedPtr last_image = nullptr;
-	float markerLength = 0.23; // [m]
+	float markerLength = 230; // [m]
 	cv::Mat cameraMatrix, distCoeffs;
 	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub;
 
@@ -54,28 +58,23 @@ private:
 		.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
 	
 	void image_cb(const sensor_msgs::msg::Image &msg){
-		// last_image = std::make_shared<sensor_msgs::msg::Image>(std::move(msg));
-		//  cv_ptr = cv_bridge::toCvCopy(msg,'BGR8' );
 		cv_ptr = cv_bridge::toCvCopy(msg, std::string("bgra8"));
 
 		/* OPENCV stuff */
 		cv::Mat inputImage;
+		cv_ptr->image.copyTo(inputImage);
 		cv::cvtColor(cv_ptr->image, inputImage, cv::COLOR_BGR2GRAY);
 		std::vector<int> markerIds;
 		std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
-		cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
-		cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-		// detectMarkers(inputImage, markerCorners, markerIds, rejectedCandidates);
-		cv::aruco::detectMarkers(inputImage, &dictionary, markerCorners, markerIds, &detectorParams,
-				rejectedCandidates);
-		
-		RCLCPP_INFO(get_logger(),"segmentation test");
+		const cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+		cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds);
 
+		// cv::Mat output_img = inputImage.clone();
+		// cv::aruco::drawDetectedMarkers(output_img, markerCorners, markerIds);
+		// cv::imwrite("camera.png", output_img);
+		
 		/* Markers pose estimation */
-		cv::Mat cameraMatrix, distCoeffs;
 		std::vector<cv::Vec3d> rvecs, tvecs;
-		// You can read camera parameters from tutorial_camera_params.yml
-		// readCameraParameters(cameraParamsFilename, cameraMatrix, distCoeffs); // This function is implemented in aruco_samples_utility.hpp
 		
 		// Set coordinate system
 		cv::Mat objPoints(4, 1, CV_32FC3);
@@ -84,14 +83,31 @@ private:
 		objPoints.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(markerLength/2.f, -markerLength/2.f, 0);
 		objPoints.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
 		
-		// Calculate pose for each marker
-		// Resulting translation is in tvecs[i] for each detected marker
-		for (int i = 0; i < int(markerCorners.size()); i++) {
-			solvePnP(objPoints, markerCorners.at(i), cameraMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
-			// RCLCPP_INFO(get_logger(), "Translation vec: %.2f %.2f %.2f", tvecs.at(i)[0],tvecs.at(i)[1],
-			// 	tvecs.at(i)[2]);
-			 RCLCPP_INFO(get_logger(),"solvePnP");
+		// cv::aruco::estimatePoseSingleMarkers(markerCorners, markerLength, cameraMatrix, distCoeffs, rvecs, tvecs);
+		
+		for (int i = 0; i < int(markerIds.size()); i++) {
+			rvecs.clear();
+			tvecs.clear();
+			cv::Vec3f rvec, tvec;
+ 			solvePnP(objPoints, markerCorners.at(i), cameraMatrix, distCoeffs, rvec, tvec);
+			rvecs.push_back(rvec);
+			tvecs.push_back(tvec);
 		}
+		
+		for (int i =0; i < int(markerIds.size()); i++){
+			RCLCPP_INFO(get_logger(), "Translation vec: %f %f %f", tvecs[i].val[0], tvecs[i].val[1],
+				tvecs[i].val[2]);
+		}
+
+		cv::Mat outputImage;
+		inputImage.copyTo(outputImage);
+		cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
+		for (int i = 0; i < rvecs.size(); ++i) {
+			auto rvec = rvecs[i];
+			auto tvec = tvecs[i];
+			cv::drawFrameAxes(outputImage, cameraMatrix, distCoeffs, rvec, tvec, 1);
+		}
+		cv::imwrite("camera.png", outputImage);
 	};
 
 
